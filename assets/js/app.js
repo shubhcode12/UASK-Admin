1cb7c65ac0924ac048cbba862249eed33968bcfe
shubhcode12 <shubhmahajan1234@gmail.com>
2022-04-23 17:21:05 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var configz = {
+    apiKey: "AIzaSyAcrUXi8uoLxziWxhlUVFGePsV5XFQZD-U",
+    authDomain: "uask-dinesh.firebaseapp.com",
+    databaseURL: "https://uask-dinesh-default-rtdb.firebaseio.com",
+    projectId: "uask-dinesh",
+    storageBucket: "uask-dinesh.appspot.com",
+    messagingSenderId: "1065064582368",
+    appId: "1:1065064582368:web:6499c7ec6e8ab084c81485",
+    measurementId: "G-YM2K2XN8FZ"
+};
+
+firebase.initializeApp(configz);
